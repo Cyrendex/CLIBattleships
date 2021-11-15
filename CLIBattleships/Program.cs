bfe87b4177d125bf3acf538a9fc4ca48c1980c66
Cyrendex <aaronunsal@gmail.com>
2021-11-15 20:22:06 +0300

    Add project files.

---
+++ CLIBattleships/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CLIBattleships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
